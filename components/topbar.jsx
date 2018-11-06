8d519382e1085d9292c5b8b27cb35ad9a1002473
Luong Quang Manh <luongquangmanh85@gmail.com>
2018-11-06 18:23:30 +0700

    Code refactoring. Improve topbar component

--- components/topbar.jsx
+++ components/topbar.jsx
@@ -1,8 +1,37 @@
+import Link from "next/link"
+import { withRouter } from 'next/router'
+
+
 const Topbar = (props) => (
-  <nav className="navbar navbar-expand-sm navbar-dark bg-primary shadow">
-    <a className="navbar-brand mb-0 h1" href="/">ALBERT</a>
+  <nav className="navbar navbar-expand-md navbar-dark bg-primary shadow">
+    <Link prefetch href="/">
+      <a className="navbar-brand mb-0 h1">
+        <strong>Albert</strong>
+      </a>
+    </Link>
+
+    <button className="navbar-toggler" data-toggle="collapse" data-target="#navbar-nav">
+      <span className="navbar-toggler-icon" />
+    </button>
+
+    <div className="collapse navbar-collapse" id="navbar-nav">
+      <div className="navbar-nav mr-auto">
+        <Link prefetch href="/">
+          <a className={`nav-item nav-link ${props.router.pathname === "/" ? "active" : ""}`}>Home</a>
+        </Link>
+        <Link prefetch href="/about">
+          <a className={`nav-item nav-link ${props.router.pathname === "/about" ? "active" : ""}`}>About</a>
+        </Link>
+      </div>
+
+      <form className="form-inline py-2 py-md-0">
+        <Link prefetch href="/signin">
+          <a className="btn btn-outline-light">Sign In</a>
+        </Link>
+      </form>
+    </div>
   </nav>
 )
 
 
-export default Topbar
+export default withRouter(Topbar)
--- configs/passport.js
+++ configs/passport.js
@@ -1,6 +1,6 @@
 const passport = require("passport")
-const JwtStrategy = require("passport-jwt").Strategy
 const ExtractJwt = require("passport-jwt").ExtractJwt
+const JwtStrategy = require("passport-jwt").Strategy
 const LocalStrategy = require("passport-local").Strategy
 const jwt = require("jsonwebtoken")
 
@@ -8,7 +8,7 @@ const User = require("../api/models/user")
 
 
 const JWT_SECRET = "albertsecret"
-const EXPIRESIN = "10h"
+const EXPIRESIN = "24h"
 // opts.issuer = 'accounts.examplesoft.com'
 // opts.audience = 'yoursite.net'
 
@@ -16,65 +16,36 @@ passport.serializeUser(function(user, done) {
   done(null, user)
 })
 
-//TODO: not populate password when query user
-passport.use(
-  "local",
-  new LocalStrategy(
-    {
-      usernameField: "email",
-      passwordField: "password",
-      session: false
-    },
-    function(username, password, done) {
-      User.findOne(
-        {
-          username: username
-        },
-        function(err, user) {
-          if (err) {
-            return done(err)
-          }
-
-          if (!user || !user.validatePassword(password)) {
-            return done(null, false)
-          }
-          token = jwt.sign({}, JWT_SECRET, {
-            audience: user.id,
-            expiresIn: EXPIRESIN
-          })
-          return done(null, user, token)
-        }
-      )
+passport.use("local", new LocalStrategy(
+  {
+    usernameField: "username",
+    passwordField: "password",
+    session: false
+  },
+  async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username })
+    } catch (err) {
+      return done(err)
     }
-  )
-)
+    if (!user || !user.validatePassword(password)) return done(null, false)
+    token = jwt.sign({}, JWT_SECRET, { audience: user.id, expiresIn: EXPIRESIN })
+    return done(null, user, token)
+  }
+))
 
-passport.use(
-  "jwt",
-  new JwtStrategy(
-    {
-      jwtFromRequest: ExtractJwt.fromHeader("authorization"),
-      secretOrKey: JWT_SECRET
-    },
-    function(jwt_payload, done) {
-      console.log("query user")
-
-      console.log(jwt_payload)
-      User.findOne(
-        {
-          id: jwt_payload.id
-        },
-        function(err, user) {
-          if (err) {
-            return done(err, false)
-          }
-          if (user) {
-            return done(null, user)
-          } else {
-            return done(null, false)
-          }
-        }
-      )
+passport.use("jwt", new JwtStrategy(
+  {
+    jwtFromRequest: ExtractJwt.fromHeader("authorization"),
+    secretOrKey: JWT_SECRET
+  },
+  async (jwt_payload, done) => {
+    try {
+      const user = await User.findOne({ id: jwt_payload.id })
+    } catch (err) {
+      return done(err, false)
     }
-  )
-)
+    if (user) return done(null, user)
+    else return done(null, false)
+  }
+))
