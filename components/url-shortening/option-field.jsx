45aa4b65cc16bd423d7daad329ef36c62c443017
Luong Quang Manh <luongquangmanh85@gmail.com>
2018-11-13 14:30:48 +0700

    Fix a minor bug. Remove now.json

--- components/url-shortening/option-field.jsx
+++
@@ -1,9 +0,0 @@
-const OptionField = (props) => (
-  <div className="form-group form-row align-items-center">
-    <label className="col-sm-4 col-form-label">{props.labelText}</label>
-    <div className="col-sm-8">{props.children}</div>
-  </div>
-)
-
-
-export default OptionField
